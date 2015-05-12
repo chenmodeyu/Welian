d560910de3fdffc547fc33984db6c40616b22fe3
liuwu <liuwugui@gmail.com>
2015-05-12 14:46:33 +0800

    系统Bug修改

---
+++ Welian/Class/Find(发现)/View/ActivityHeaderViewCell.h
@@ -0,0 +1,15 @@
+//
+//  ActivityHeaderViewCell.h
+//  Welian
+//
+//  Created by weLian on 15/5/12.
+//  Copyright (c) 2015年 chuansongmen. All rights reserved.
+//
+
+#import "BaseTableViewCell.h"
+
+@interface ActivityHeaderViewCell : BaseTableViewCell
+
+@property (assign,nonatomic) UILabel *titleLabel;
+
+@end
